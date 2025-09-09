2c9412c1d0460278b233a5eb14f8890c08f5a890
FBIK. <FBIKdot@users.noreply.github.com>
2025-09-09 14:26:52 +0800

    chore(vitepress): add config

---
+++ .vitepress/config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+  srcDir: "./",
+  srcExclude: ["node_modules/**/*"],
+  rewrites: {
+    "README.md": "index.md",
+  },
+  themeConfig: {
+    siteTitle: "每周科技补全",
+    nav: [
+      {
+        text: "Bilibili",
+        link: "https://space.bilibili.com/67079745/lists/3173076",
+      },
+      { text: "Github", link: "https://github.com/xuanli199/weekly" },
+    ],
+  },
+})
