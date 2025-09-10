2f200f84a0b2d690ccd78e23e0f1c9c71e5901fc
FBIK. <FBIKdot@users.noreply.github.com>
2025-09-10 19:42:18 +0800

    chore(vitepress): use rss feed

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from "vitepress";
+import { RSSOptions, RssPlugin } from "vitepress-plugin-rss";
+const RSS: RSSOptions = {
+  title: "玄离199 每周科技补全",
+  baseUrl: "https://xuanli199.github.io/weekly",
+  copyright: "玄离199",
+  author: { name: '"玄离199"' },
+  filter: (post) => !post.filepath.includes("README.md"),
+};
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
@@ -9,13 +17,16 @@ export default defineConfig({
     "README.md": "index.md",
   },
   themeConfig: {
+    socialLinks: [{
+      icon: "github",
+      link: "https://github.com/xuanli199/weekly",
+    }, {
+      icon: "bilibili",
+      link: "https://space.bilibili.com/67079745/lists/3173076",
+    }],
     siteTitle: "每周科技补全",
-    nav: [
-      {
-        text: "Bilibili",
-        link: "https://space.bilibili.com/67079745/lists/3173076",
-      },
-      { text: "Github", link: "https://github.com/xuanli199/weekly" },
-    ],
+  },
+  vite: {
+    plugins: [RssPlugin(RSS)],
   },
 });
