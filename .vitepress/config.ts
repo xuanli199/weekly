4f168aaf104857d28e3441bb841179e9b7e44063
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-09-14 13:42:20 +0800

    style: format code

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitepress";
 import { RSSOptions, RssPlugin } from "vitepress-plugin-rss";
+
 const RSS: RSSOptions = {
   title: "玄离199 每周科技补全",
   baseUrl: "https://xuanli199.github.io",
@@ -17,19 +18,22 @@ export default defineConfig({
     "README.md": "index.md",
   },
   themeConfig: {
-    socialLinks: [{
-      icon: "github",
-      link: "https://github.com/xuanli199/weekly",
-    }, {
-      icon: "bilibili",
-      link: "https://space.bilibili.com/67079745/lists/3173076",
-    }],
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/xuanli199/weekly",
+      },
+      {
+        icon: "bilibili",
+        link: "https://space.bilibili.com/67079745/lists/3173076",
+      },
+    ],
     siteTitle: "每周科技补全",
 
     // VitePress 自带的检索全文功能。
     // See: https://vitepress.dev/zh/reference/default-theme-search
     search: {
-      provider: 'local',
+      provider: "local",
       options: {
         miniSearch: {
           options: {
@@ -39,12 +43,12 @@ export default defineConfig({
               // 这种拆分方式可以在遇到英数字时以单词拆分，
               // 这样做是为了更全面地匹配非英数字的内容，
               // 同时避免单字母或单数字拆分造成的无意义匹配。
-              return text.match(/[A-Za-z0-9]+|./g)?.filter(Boolean) ?? []
+              return text.match(/[A-Za-z0-9]+|./g)?.filter(Boolean) ?? [];
             },
           },
           searchOptions: {
             // 完整匹配检索关键字（避免检索单词时出现单字内容）。
-            combineWith: 'AND',
+            combineWith: "AND",
           },
         },
       },
